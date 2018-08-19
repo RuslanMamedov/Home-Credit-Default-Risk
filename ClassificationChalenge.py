b2369691e71ff9ff484fb9af7c87ce4c5facd2b4
RuslanMamedov <41539922+RuslanMamedov@users.noreply.github.com>
2018-08-19 07:49:32 -0700

    Update ClassificationChalenge.py

--- ClassificationChalenge.py
+++ ClassificationChalenge.py
@@ -1,72 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  6 13:51:08 2018
-
-@author: Ruslan Mamedov
-"""
-#Classification Challenge
-
-#Importing the libraries
-import pandas as pd
-import numpy as np
-
-#importing the dataset
-dataset_train=pd.read_csv('application_train.csv')
-dataset_test =pd.read_csv('application_test.csv')
-dataset_train.head()
-
-#replacing nonumeric binary columns with numeric values
-dataset_train['FLAG_OWN_REALTY'].replace(('Y', 'N'), (1, 0), inplace=True)
-dataset_train['FLAG_OWN_CAR'].replace(('Y', 'N'), (1, 0), inplace=True)
-dataset_train['EMERGENCYSTATE_MODE'].replace(('No', 'Yes'), (1, 0), inplace=True)
-dataset_train['CODE_GENDER'].replace(('M', 'F'), (0, 1), inplace=True)
-
-dataset_test['FLAG_OWN_REALTY'].replace(('Y', 'N'), (1, 0), inplace=True)
-dataset_test['FLAG_OWN_CAR'].replace(('Y', 'N'), (1, 0), inplace=True)
-dataset_test['EMERGENCYSTATE_MODE'].replace(('No', 'Yes'), (1, 0), inplace=True)
-dataset_test['CODE_GENDER'].replace
-
-#there are 13 nonnumeric columns; will have to convert them with one hot encoder. Then we'll take care of the missing values
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-labelencoder=LabelEncoder()
-onehotencoder=OneHotEncoder()
-for column in range(0-121):
-    if dataset_train[column].type=='object':
-        dataset_train[column]=labelencoder.fit_transform[dataset_test[column]]
-    if dataset_test[column].type=='object':
-        dataset_test[column]=labelencoder.fit_transform[dataset_test[column]]
-        
-dataset_train=pd.get_dummies(dataset_train)
-dataset_test=pd.get_dummies(dataset_test)
-
-#imputing missing values with a mean
-dataset_train=dataset_train.fillna(dataset_train.mean())
-dataset_test=dataset_test.fillna(dataset_test.mean())
-
-# Dimension Reduction - dropping features with less than 2% correllation
-correlations = dataset_train.corr()['TARGET'].sort_values()
-correlations = correlations.reset_index().values
-print(dataset_train.shape)
-for column in range (0,len(correlations)-1):
-    if correlations[column,1]>-0.02 and correlations[column,1]<0.02 and correlations[column,0]!='SK_ID_CURR':
-        dataset_train=dataset_train.drop ([correlations[column,0]], axis=1)
-print(dataset_train.shape)
-correlations = dataset_train.corr()['TARGET'].sort_values()
-print('Most Positive Correlations:\n', correlations.tail(15))
-print('\nMost Negative Correlations:\n', correlations.head(15))
-
-#Splitting train dataset, training the model
-y=dataset_train.iloc[:,dataset_train.columns.get_loc('TARGET')].values
-X=dataset_train.drop(['TARGET'], axis=1).values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)# -*- coding: utf-8 -*-
 """
 Created on Mon Aug  6 13:51:08 2018
 @author: Ruslan Mamedov
@@ -120,7 +51,7 @@ dataset_train, dataset_test = dataset_train.align(dataset_test, join = 'inner', 
 # Add the target back in
 dataset_train['TARGET'] = train_labels
 
-# Dimension Reduction - dropping features with less than 2% correllation
+# Dimension Reduction - dropping the features with less than 2% correllation
 correlations = dataset_train.corr()['TARGET'].sort_values()
 correlations = correlations.reset_index().values
 print(dataset_train.shape)
